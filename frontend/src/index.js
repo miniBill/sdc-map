6cf54626facb9a8579622553e04a283ad4265d5b
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-08-27 00:04:15 +0200

    Moving towards correct encryption

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,29 @@
 import { box, randomBytes } from "./nacl-fast.js";
 import { decodeUTF8, encodeBase64 } from "./nacl-util.js";
 
+const userPair = box.keyPair();
+const serverPair = box.keyPair();
+
 function encrypt(input, publicKey, secretKey) {
   const nonce = randomBytes(box.nonceLength);
   const messageUint8 = decodeUTF8(input);
   const encrypted = box(messageUint8, nonce, publicKey, secretKey);
 
-  const fullMessage = new Uint8Array(nonce.length + encrypted.length);
+  const fullMessage = new Uint8Array(
+    nonce.length + encrypted.length + userPair.publicKey.length
+  );
   fullMessage.set(nonce);
   fullMessage.set(encrypted, nonce.length);
+  fullMessage.set(userPair.publicKey, nonce.length + encrypted.length);
 
   return encodeBase64(fullMessage);
 }
-
-const userPair = box.keyPair();
-const serverPair = box.keyPair();
 
 const app = Elm.Main.init({
   node: document.querySelector("main"),
 });
 app.ports.encrypt.subscribe(function (input) {
   app.ports.encrypted.send(
-    encrypt(JSON.stringify(input), userPair.publicKey, serverPair.secretKey)
+    encrypt(JSON.stringify(input), serverPair.publicKey, userPair.secretKey)
   );
 });

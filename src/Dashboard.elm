384b1410264aa995f11d2dd7c36128dfce7eec9d
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-08-28 11:38:17 +0200

    Add pie visualization

--- src/Dashboard.elm
+++ src/Dashboard.elm
@@ -4,6 +4,7 @@ import Element exposing (Column, Element, alignTop, centerY, el, fill, shrink, t
 import Element.Border as Border
 import Element.Font as Font
 import List.Extra
+import Pie
 import Set exposing (Set)
 import Theme
 import Types exposing (FrontendMsg(..), Input)
@@ -29,87 +30,102 @@ view invalidCaptchas inputs =
             [ width fill
             , alignTop
             ]
-            [ Theme.column
-                [ Border.width 1
-                , Theme.padding
-                , width fill
-                ]
-                [ el [ Font.bold ] <| text "On map"
-                , Element.table [ Theme.spacing ]
-                    { data =
-                        validInputs
-                            |> List.filter (\{ nameOnMap } -> nameOnMap == Just True)
-                    , columns =
-                        [ column "Name" .name
-                        , column "Country" .country
-                        , column "Location" .location
-                        ]
-                    }
-                ]
-            , Theme.column
-                [ Border.width 1
-                , Theme.padding
-                , width fill
-                ]
-                [ el [ Font.bold ] <| text "Statistics by country"
-                , Element.table [ Theme.spacing ]
-                    { data =
-                        validInputs
-                            |> List.Extra.gatherEqualsBy (\{ country } -> country)
-                            |> List.map
-                                (\( { country }, rest ) ->
-                                    { country = country
-                                    , count = List.length rest + 1
-                                    }
-                                )
-                            |> List.sortBy (\{ count } -> -count)
-                    , columns =
-                        [ column "Country" .country
-                        , column "Count" <| \{ count } -> String.fromInt count
-                        ]
-                    }
-                ]
-            ]
-        , Theme.column
-            [ Border.width 1
-            , Theme.padding
-            , alignTop
-            ]
-            [ el [ Font.bold ] <| text "Captchas"
-            , Element.table [ Theme.spacing ]
+            [ card "On map"
                 { data =
-                    inputs
-                        |> List.Extra.gatherEqualsBy (\{ captcha } -> captcha)
+                    validInputs
+                        |> List.filter (\{ nameOnMap } -> nameOnMap == Just True)
+                , columns =
+                    [ column "Name" .name
+                    , column "Country" .country
+                    , column "Location" .location
+                    ]
+                , pie = Nothing
+                }
+            , card "Statistics by country"
+                { data =
+                    validInputs
+                        |> List.Extra.gatherEqualsBy (\{ country } -> country)
                         |> List.map
-                            (\( { captcha }, rest ) ->
-                                { captcha = captcha
+                            (\( { country }, rest ) ->
+                                { country = country
                                 , count = List.length rest + 1
                                 }
                             )
                         |> List.sortBy (\{ count } -> -count)
                 , columns =
-                    [ column "Captcha" .captcha
+                    [ column "Country" .country
                     , column "Count" <| \{ count } -> String.fromInt count
-                    , { header = el [ Font.bold ] <| text "Is valid"
-                      , view =
-                            \row ->
-                                let
-                                    invalid : Bool
-                                    invalid =
-                                        Set.member row.captcha invalidCaptchas
-                                in
-                                Theme.button []
-                                    { onPress = Just <| CaptchaIsValid row.captcha invalid
-                                    , label =
-                                        if invalid then
-                                            text "No"
-
-                                        else
-                                            text "Yes"
-                                    }
-                      , width = shrink
-                      }
                     ]
+                , pie = Just <| \{ country, count } -> ( country, toFloat count )
                 }
             ]
+        , card "Captchas"
+            { data =
+                inputs
+                    |> List.Extra.gatherEqualsBy (\{ captcha } -> captcha)
+                    |> List.map
+                        (\( { captcha }, rest ) ->
+                            { captcha = captcha
+                            , count = List.length rest + 1
+                            }
+                        )
+                    |> List.sortBy (\{ count } -> -count)
+            , columns =
+                [ column "Captcha" .captcha
+                , column "Count" <| \{ count } -> String.fromInt count
+                , { header = el [ Font.bold ] <| text "Is valid"
+                  , view =
+                        \{ captcha } ->
+                            let
+                                invalid : Bool
+                                invalid =
+                                    Set.member captcha invalidCaptchas
+                            in
+                            Theme.button []
+                                { onPress = Just <| CaptchaIsValid captcha invalid
+                                , label =
+                                    if invalid then
+                                        text "No"
+
+                                    else
+                                        text "Yes"
+                                }
+                  , width = shrink
+                  }
+                ]
+            , pie = Nothing
+            }
+        ]
+
+
+card :
+    String
+    ->
+        { data : List record
+        , columns : List (Column record msg)
+        , pie : Maybe (record -> ( String, Float ))
+        }
+    -> Element msg
+card label { data, columns, pie } =
+    Theme.column
+        [ Border.width 1
+        , Theme.padding
+        , width fill
+        , alignTop
+        ]
+        [ el [ Font.bold ] <| text label
+        , Element.table [ Theme.spacing ]
+            { data = data
+            , columns = columns
+            }
+        , case pie of
+            Nothing ->
+                Element.none
+
+            Just f ->
+                data
+                    |> List.map f
+                    |> List.sortBy (\( _, count ) -> count)
+                    |> Pie.view
+                    |> Element.html
         ]
---
+++ src/Pie.elm
@@ -0,0 +1,87 @@
+module Pie exposing (view)
+
+import Array exposing (Array)
+import Color exposing (Color)
+import Color.Oklch
+import Path
+import Shape exposing (defaultPieConfig)
+import TypedSvg exposing (g, svg, text_)
+import TypedSvg.Attributes exposing (dy, fill, fontSize, stroke, textAnchor, transform, viewBox)
+import TypedSvg.Core exposing (Svg, text)
+import TypedSvg.Types exposing (AnchorAlignment(..), Paint(..), Transform(..), em, px)
+
+
+w : Float
+w =
+    900
+
+
+h : Float
+h =
+    900
+
+
+radius : Float
+radius =
+    min w h / 2
+
+
+pieSlice : Array Color -> Int -> Shape.Arc -> Svg msg
+pieSlice colors index datum =
+    Path.element (Shape.arc datum)
+        [ fill <| Paint <| Maybe.withDefault Color.black <| Array.get index colors
+        , stroke <| Paint Color.white
+        ]
+
+
+pieLabel : Shape.Arc -> ( String, Float ) -> Svg msg
+pieLabel slice ( label, _ ) =
+    let
+        ( x, y ) =
+            Shape.centroid
+                { slice
+                    | innerRadius = radius * 2 / 3
+                    , outerRadius = radius * 2 / 3
+                }
+    in
+    text_
+        [ transform [ Translate x y ]
+        , dy (em 0.35)
+        , textAnchor AnchorMiddle
+        ]
+        [ text label ]
+
+
+view : List ( String, Float ) -> Svg msg
+view model =
+    let
+        pieData : List Shape.Arc
+        pieData =
+            model
+                |> List.map Tuple.second
+                |> Shape.pie { defaultPieConfig | outerRadius = radius }
+
+        colors : Array Color
+        colors =
+            let
+                length : Int
+                length =
+                    List.length model
+            in
+            List.range 0 (length - 1)
+                |> List.map
+                    (\i ->
+                        Color.Oklch.oklch 0.8 0.1 (toFloat i / toFloat length)
+                            |> Color.Oklch.toColor
+                    )
+                |> Array.fromList
+    in
+    svg [ viewBox 0 0 w h ]
+        [ g
+            [ transform [ Translate (w / 2) (h / 2) ]
+            , fontSize (px <| min w h / 20)
+            ]
+            [ g [] <| List.indexedMap (pieSlice colors) pieData
+            , g [] <| List.map2 pieLabel pieData model
+            ]
+        ]

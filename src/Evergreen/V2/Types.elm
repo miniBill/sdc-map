bbfbac46d63fb32e333e20fcc6e60b7734434fc4
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-08-28 10:39:57 +0200

    V2: no RPC

---
+++ src/Evergreen/V2/Types.elm
@@ -0,0 +1,71 @@
+module Evergreen.V2.Types exposing (..)
+
+import Dict
+import Random
+
+
+type alias Input =
+    { name : String
+    , country : String
+    , location : String
+    , nameOnMap : Maybe Bool
+    , id : String
+    , captcha : String
+    }
+
+
+type alias Error =
+    String
+
+
+type EncryptedString
+    = EncryptedString String
+
+
+type FrontendModel
+    = Filling Input (Maybe Error)
+    | Encrypting Input
+    | Submitting Input
+    | Submitted
+        { id : String
+        , input : Input
+        }
+    | AdminDecrypting String (Dict.Dict String EncryptedString)
+    | AdminDecrypted (List Input)
+
+
+type alias BackendModel =
+    { seed : Random.Seed
+    , submissions : Dict.Dict String EncryptedString
+    }
+
+
+type FrontendMsg
+    = Name String
+    | Country String
+    | Location String
+    | NameOnMap Bool
+    | Id String
+    | Captcha String
+    | Submit
+    | Encrypted EncryptedString
+    | AdminSecretKey String
+    | Decrypt
+    | Decrypted (List String)
+    | Nop
+
+
+type ToBackend
+    = TBSubmit EncryptedString
+    | TBAdmin String
+
+
+type alias BackendMsg =
+    {}
+
+
+type ToFrontend
+    = TFSubmitted
+        { id : String
+        }
+    | TFAdmin (Dict.Dict String EncryptedString)
